1b1584707e3987e0ce50fbf9f1757f11a702d071
Ayodele David Oluwatobi <dayodele89@outlook.com>
2025-09-22 20:39:37 +0100

    Implemented Splash Screen

--- app/src/main/java/com/example/ainotes/MainActivity.kt
+++ app/src/main/java/com/example/ainotes/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AINotesTheme {
-                RecordTranscribeScreen()
+                SplashScreen()
             }
         }
     }
--- app/src/main/java/com/example/ainotes/ui/theme/Color.kt
+++ app/src/main/java/com/example/ainotes/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Splash Screen Colors
+val DarkNavyBackground = Color(0xFF1E293B)
+val BlueAccent = Color(0xFF3B82F6)
+val WhiteText = Color(0xFFFFFFFF)
+val LightGrayText = Color(0xFFCBD5E1)
